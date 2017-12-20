a780b9f1ca2de7792163afcecc64ef28cc0a58ec
Bug38 <lguimard38@gmail.com>
2017-12-20 13:51:50 +0100

    2017: add 20-2.

--- 2017/20/20-2.lua
+++ 2017/20/20-2.lua
@@ -1,21 +1,14 @@
 local particules = {}
 
 for line in io.lines("input20.txt") do
-  --p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>
   local pX, pY, pZ, vX, vY, vZ, aX, aY, aZ = line:match("p=<([-%d]+),([-%d]+),([-%d]+)>, v=<([-%d]+),([-%d]+),([-%d]+)>, a=<([-%d]+),([-%d]+),([-%d]+)>")
   local part = {
     coords = { pX,pY,pZ },
     velocity = { vX,vY,vZ },
     acceleration = { aX,aY,aZ },
-    --dist = math.abs(pX) + math.abs(pY) + math.abs(pZ),
   }
   table.insert(particules, part)
 end
-
---local oldNearest = 1
---local nearest
-local deleted = 0
-local oldDeleted = 0
 
 function areEqualCoords(c1, c2)
   return c1[1] == c2[1] and c1[2] == c2[2] and c1[3] == c2[3]
@@ -23,7 +16,6 @@ end
 
 function removeParticules()
   local toRemove = {}
-  -- Remove particules that collide
   for i = 1, #particules do
     local toDelete = false
     for j = i+1, #particules do
@@ -45,26 +37,16 @@ end
 function newVelocity(v,a)
   local vX,vY,vZ = unpack(v)
   local aX,aY,aZ = unpack(a)
-  --print(vX,vY,vZ)
-  --print(aX,aY,aZ)
-  vX,vY,vZ = vX+aX,vY+aY,vZ+aZ
-  --print(vX,vY,vZ)
-  return vX,vY,vZ
+  return vX+aX,vY+aY,vZ+aZ
 end
 function newCoords(p,v)
   local pX,pY,pZ = unpack(p)
   local vX,vY,vZ = unpack(v)
-  --print(pX,pY,pZ)
-  --print(vX,vY,vZ)
-  pX,pY,pZ = pX+vX,pY+vY,pZ+vZ
-  --print(pX,pY,pZ)
-  return pX,pY,pZ
+  return pX+vX,pY+vY,pZ+vZ
 end
 
 for g=1,100 do
-  --local miniDist = particules[1].dist
   removeParticules()
-  -- Calculate new pos
   for i = 1, #particules do
     if particules[i] then
       local aX, aY, aZ = unpack(particules[i].acceleration)
@@ -73,22 +55,7 @@ for g=1,100 do
 
       particules[i].velocity = {newVelocity(particules[i].velocity,particules[i].acceleration)}
       particules[i].coords = {newCoords(particules[i].coords,particules[i].velocity)}
-
-      --particules[i].dist = math.abs(particules[i].coords[1]) + math.abs(particules[i].coords[2]) + math.abs(particules[i].coords[3])
-
-      --if particules[i].dist < miniDist then
-      --  nearest = i
-      --  miniDist = particules[i].dist
-      --end
     end
-  end
-  --if oldNearest ~= nearest then
-  --  print("Particule " .. nearest .. " at distance " .. miniDist .. "(old was " .. oldNearest .. ")")
-  --end
-  --oldNearest = nearest
-  if oldDeleted ~= deleted then
-    print("Particules : " .. #particules - deleted)
-    oldDeleted = deleted
   end
 end
 
